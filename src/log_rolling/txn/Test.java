c817f21c462b4fbafb3cd7b65197224bd91faa88
seonjiwon <tommys915@gmail.com>
2026-02-04 12:37:37 +0900

    feature/txnLog

--- src/log_rolling/txn/Test.java
+++ src/log_rolling/txn/Test.java
@@ -1,10 +1,51 @@
 package log_rolling.txn;
 
+import java.util.UUID;
+
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		TransactionAuditLogger logger = new TransactionAuditLogger();
+		
+		for (int i = 1; i <= 5000; i++) {
+            TxnLog t = randomTxn(i);
+            logger.log(t);
+            Thread.sleep(5);
+        }
+		
+
+        System.out.println("Done. Check logs/txn/txn.log and archive folder.");
 
 	}
+	
+	private static TxnLog randomTxn(int i) {
+	    String txnId = UUID.randomUUID().toString();
+	    String username = "user" + (i % 10);
+
+	    long amount = (long) (Math.random() * 100_000) + 1;
+
+	    String rawAccount = "3333" + "-" + (int)(Math.random() * 1_000_000 + 1) + "-" + (int)(Math.random() * 1_000_000 + 1);
+	    String accountMasked = maskAccount(rawAccount);
+
+	    boolean success = Math.random() < 0.8;
+	    String result = success ? "SUCCESS" : "FAIL";
+
+	    return new TxnLog(
+	            txnId,
+	            username,
+	            amount,
+	            accountMasked,
+	            result
+	    );
+	}
+
+	private static String maskAccount(String raw) {
+        String[] parts = raw.split("-");
+
+        String masked = parts[0] + "-" + "*".repeat(parts[1].length()) + "-" + parts[2];
+        return masked;
+    }
+	
+
 
 }
--- src/log_rolling/txn/TxnLog.java
+++ src/log_rolling/txn/TxnLog.java
@@ -1,12 +1,40 @@
 package log_rolling.txn;
 
 public class TxnLog {
-    private String txnId;          // UUID
-    private String username;       // 이미 넣었다고 했으니 OK
-    private long amount;           // 랜덤
-    private String accountMasked;  // 12****34
-    private String result;         // SUCCESS / FAIL
-    private String errorCode;      // 실패 시만 값
-    private long timestamp;        // System.currentTimeMillis()
+	private String txnId; // UUID
+	private String username; // 이미 넣었다고 했으니 OK
+	private long amount; // 랜덤
+	private String accountMasked; // 12****34
+	private String result; // SUCCESS / FAIL
+
+	public String getTxnId() {
+		return txnId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public String getAccountMasked() {
+		return accountMasked;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+
+	public TxnLog(String txnId, String username, long amount, String accountMasked, String result) {
+		super();
+		this.txnId = txnId;
+		this.username = username;
+		this.amount = amount;
+		this.accountMasked = accountMasked;
+		this.result = result;
+	}
 
 }
